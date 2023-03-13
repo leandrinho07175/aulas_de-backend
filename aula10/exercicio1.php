4bc884dce3fd141c29badf6cc2a0feb380dd484c
Leandro Ferreira de Oliveira <14068040613@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-13 08:41:46 -0300

    exercicios vetor

---
+++ aula10/exercicio1.php
@@ -0,0 +1,11 @@
+<?php
+// criar o vetor abaixo, apos imprimir seus valores utilizando a estrutura de repetição for.
+    $valores = [];
+    $valores[] = "Pedro";
+    $valores[] = "Julin";
+    $valores[] = "Maria";
+    
+    for($i = 0; $i <= 3; $i++){
+    echo $valores[$i] . "<br>";
+    }
+?>
---
+++ aula10/exercicio2.php
@@ -0,0 +1,19 @@
+<h2>exercicio 2</h2>
+<pre>
+       +---------+
+nome   |maria |
+       +---------+
+idade  |  17   |
+       +---------+
+ altura | 1.65   |
+       +---------+
+</pre>
+
+<?php
+   $valores = [
+        "nome" => maria,
+        "idade" => 17,
+        "altura" => 1.65
+   ];
+
+   
